2bb3d8fd73078c2147bc143ecc3fc70656d28276
Joel Palmer <joelpalmer@users.noreply.github.com>
2018-07-30 11:31:01 -0500

    Add poop icons for ratings

--- src/components/shit/ShitListItem.js
+++ src/components/shit/ShitListItem.js
@@ -5,11 +5,16 @@ export default class ShitListItem extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    let shitVect = [];
+    for (let i = 0; i < this.props.rating; i++) {
+      shitVect.push(<span>💩</span>);
+    }
     return (
       <tr>
         <td>{this.props.name}</td>
-        <td>{this.props.rating}</td>
+        <td>{shitVect}</td>
       </tr>
     );
   }
